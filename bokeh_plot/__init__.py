41af5e415f1e275b6d6f6e7e249aea8ee1488b66
Lev Maximov <lev.maximov@gmail.com>
2020-03-05 18:45:46 +0700

    tuple/list in style

--- bokeh_plot/__init__.py
+++ bokeh_plot/__init__.py
@@ -45,16 +45,24 @@ def is_2d(y):
     return isinstance(y, torch.Tensor) and y.dim()==2 or \
       not isinstance(y, torch.Tensor) and isinstance(y, Iterable) and len(y) and isinstance(y[0], Iterable)
 
+class ParseError(Exception):
+    pass
+
 def parse(x, y, style):
     tr = []
     if is_2d(y):
         n = len(y)
-        line_style = re.sub('[a-z]', '', style) or '-'
-        colors = re.sub('[^a-z]', '', style) or 'b'
-        if len(colors) == n:
-            styles = [line_style+c for c in colors]
+        if isinstance(style, (tuple, list)):
+            styles = style
+            if len(styles) != n:
+                raise ParseError('len(styles)={len(styles)} does not match len(y)={len(y)}')
         else:
-            styles = [line_style+colors]*n
+            line_style = re.sub('[a-z]', '', style) or '-'
+            colors = re.sub('[^a-z]', '', style) or 'b'
+            if len(colors) == n:
+                styles = [line_style+c for c in colors]
+            else:
+                styles = [line_style+colors]*n
         if is_2d(x):
             for xi, yi, si in zip(x, y, styles):
                 tr.append((xi, yi, si))
@@ -72,53 +80,58 @@ def parse(x, y, style):
     return tr
 
 def plot(*args, p=None, hover=False, mode='plot', **kwargs):
-    show = p is None
-    if show:
-        if mode == 'plot':
-            p = figure()
-        elif mode == 'semilogx':
-            p = figure(x_axis_type='log')
-        elif mode == 'semilogy':
-            p = figure(y_axis_type='log')
-        elif mode == 'loglog':
-            p = figure(x_axis_type='log', y_axis_type='log')
-            print('ok')
-    notebook_handle = kwargs.pop('notebook_handle', False)
-    if hover:
-        p.add_tools(HoverTool(tooltips = [("x", "@x"),("y", "@y")]))
-    tr = []
-    style = '-'
-    if len(args) in (1, 2):
-        x = None
-        if len(args) == 1:
-            y = args[0]
-        elif isinstance(args[1], str):
-            y, style = args
-        else:
-            x, y = args
-        tr.extend(parse(x, y, style))
-    elif len(args) % 3 == 0:
-        for h in range(len(args)//3):
-            x, y, style = args[3*h:3*(h+1)]
+    try:
+        show = p is None
+        if show:
+            if mode == 'plot':
+                p = figure()
+            elif mode == 'semilogx':
+                p = figure(x_axis_type='log')
+            elif mode == 'semilogy':
+                p = figure(y_axis_type='log')
+            elif mode == 'loglog':
+                p = figure(x_axis_type='log', y_axis_type='log')
+                print('ok')
+        notebook_handle = kwargs.pop('notebook_handle', False)
+        if hover:
+            p.add_tools(HoverTool(tooltips = [("x", "@x"),("y", "@y")]))
+        tr = []
+        style = '-'
+        if len(args) in (1, 2):
+            x = None
+            if len(args) == 1:
+                y = args[0]
+            elif isinstance(args[1], str) or \
+                 isinstance(args[1], (tuple, list)) and len(args[0]) > 0 and \
+                 isinstance(args[1][0], str):
+                y, style = args
+            else:
+                x, y = args
             tr.extend(parse(x, y, style))
-    base_color = kwargs.pop('color', BLUE)
-    for x, y, style in tr:
-        if isinstance(y, torch.Tensor):
-            y = y.detach().numpy()
-        source = ColumnDataSource(data=dict(x=x, y=y))
-        if style and style[-1] in COLORS:
-            color = COLORS[style[-1]]
-            style = style[:-1]
-        else:
-            color = base_color
-        if not style or '-' in style:
-            p.line('x', 'y', source=source, color=color, **kwargs)
-        if '.' in style:
-            p.circle('x', 'y', source=source, color=color, **kwargs)
-    handle = None
-    if show:
-        handle = bp.show(p, notebook_handle=notebook_handle)
-    return source if notebook_handle else None
+        elif len(args) % 3 == 0:
+            for h in range(len(args)//3):
+                x, y, style = args[3*h:3*(h+1)]
+                tr.extend(parse(x, y, style))
+        base_color = kwargs.pop('color', BLUE)
+        for x, y, style in tr:
+            if isinstance(y, torch.Tensor):
+                y = y.detach().numpy()
+            source = ColumnDataSource(data=dict(x=x, y=y))
+            if style and style[-1] in COLORS:
+                color = COLORS[style[-1]]
+                style = style[:-1]
+            else:
+                color = base_color
+            if not style or '-' in style:
+                p.line('x', 'y', source=source, color=color, **kwargs)
+            if '.' in style:
+                p.circle('x', 'y', source=source, color=color, **kwargs)
+        handle = None
+        if show:
+            handle = bp.show(p, notebook_handle=notebook_handle)
+        return source if notebook_handle else None
+    except ParseError as e:
+        print(e)
 
 # eg:
 # plot([1,2,3])
@@ -149,7 +162,7 @@ colormap =cm.get_cmap("viridis")
 bokehpalette = [matplotlib.colors.rgb2hex(m) for m in colormap(np.arange(colormap.N))]
 def imshow(im):
     p = figure()
-    p.image([np.flipud(im)], x=[0], y=[0], dw=[im.shape[1]], dh=[im.shape[0]], palette=bokehpalette)
+    p.image([im], x=[0], y=[0], dw=[im.shape[1]], dh=[im.shape[0]], palette=bokehpalette)
     bp.show(p)
 
 def load_ipython_extension(ipython):
