4c5d67589542eb939352503dd16c9a9968384c58
Lev Maximov <lev.maximov@gmail.com>
2023-03-07 13:04:50 +0700

    version bump to 0.2.6, bokeh<3 in requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bokehlab",
-    version="0.2.5",
+    version="0.2.6",
     author='Lev Maximov',
     author_email='lev.maximov@gmail.com',
     url='https://github.com/axil/bokehlab',
