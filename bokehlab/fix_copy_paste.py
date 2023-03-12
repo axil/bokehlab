59780c1c089081452301680bdb850c6f4bd17d65
Lev Maximov <lev.maximov@gmail.com>
2023-03-13 01:16:04 +0700

    --dry-run for fix_copy_paste

--- bokehlab/fix_copy_paste.py
+++ bokehlab/fix_copy_paste.py
@@ -5,15 +5,16 @@ from shutil import move
 
 import bokeh
 
-os.chdir(Path(bokeh.__file__).parent / 'server' / 'static' / 'js')
+static_dir = Path(bokeh.__file__).parent / 'server' / 'static' / 'js'
+os.chdir(static_dir)
 txt = open('bokeh.min.js', newline='').read()
 context = 'n=(0,u._resolve_root_elements)(e);(0,f.add_document_standalone)(t,o,n)'
 fix = ";n[0].removeAttribute('id')"
 if context+fix in txt:
-    print('Already patched')
+    print(f'File {static_dir/"bokeh.min.js"} is already patched')
 elif context in txt:
-    if sys.argv[1] in ('-d', '--dry-run'):
-        print('Patchable (dry run)')
+    if len(sys.argv) > 1 and sys.argv[1] in ('-d', '--dry-run'):
+        print(f'File {static_dir/"bokeh.min.js"}, patch applicable (dry run)')
     else:
         move('bokeh.min.js', 'bokeh.min.js.bak')
         txt = txt.replace(context, context + fix, 1)
